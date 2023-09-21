e1d5cbbeef8bbd9822bfd9c193b3dea9b8f925d4
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-09-21 15:21:55 +0300

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,19 +21,40 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.serialize() for bakery in bakeries]
+    return jsonify(serialized_bakeries)
+
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        serialized_bakery = bakery.serialize()
+        return jsonify(serialized_bakery)
+    else:
+        return jsonify({'error': 'Bakery not found'}), 404
+    
+
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+     baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+     serialized_baked_goods = [bg.serialize() for bg in baked_goods]
+     return jsonify(serialized_baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        serialized_most_expensive = most_expensive.serialize()
+        return jsonify(serialized_most_expensive)
+    else:
+        return jsonify({'error': 'No baked goods found'}), 404
+    
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
--- server/models.py
+++ server/models.py
@@ -12,9 +12,39 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship("BakedGood", back_populates="bakery")
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'baked_goods': [baked_good.serialize() for baked_good in self.baked_goods]
+        }
+    
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery = db.relationship("Bakery", back_populates="baked_goods")
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        }
